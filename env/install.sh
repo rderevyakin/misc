3512c2d8c435129f048fa408ecb69e8f38440c14
ruslan <rderevakin@gmail.com>
2015-03-21 10:06:41 +0300

    initial version

---
+++ env/install.sh
@@ -0,0 +1,20 @@
+# sudo apt-get install git
+# sudo apt-get install curl
+
+# zsh
+#
+
+# solarized theme
+#
+
+# rbenv
+# git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+# ruby & rails
+#
+
+# mysql
+# sudo apt-get install mysql-server-5.6 mysql-client-5.6
+
+# other
+# sudo apt-get install htop
