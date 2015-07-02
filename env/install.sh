d6b5a175843f6e3a4cca7e4fed82f71ad2ed50ee
Ruslan Derevyakin <rderevyakin@gmail.com>
2015-07-02 22:07:45 +0300

    raw commented scripts

--- env/install.sh
+++ env/install.sh
@@ -1,20 +1,46 @@
 # sudo apt-get install git
 # sudo apt-get install curl
+sudo apt-get install ctags
+
+# caps lock remap to esc
+# current_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# swap_conf=$current_dir'/caps_to_esc_swap'
+# echo 'xmodmap '$swap_conf >> ~/.profile
 
 # zsh
-#
+# sudo apt-get install zsh
+# wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+# chsh -s /bin/zsh
+# zsh
+
+# advanced grep
+# sudo apt-get install silversearcher-ag
 
 # solarized theme
 #
 
 # rbenv
 # git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+# echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+# cd $HOME
+# exec $SHELL
+# source $HOME/.zshrc
+# git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# rbenv rehash
 
 # ruby & rails
-#
+# sudo apt-get install libssl-dev autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+# rbenv install 2.1.2
+# rbenv rehash
+# rbenv global 2.1.2
+# gem install bundler
+
+# vim from sources
 
 # mysql
 # sudo apt-get install mysql-server-5.6 mysql-client-5.6
+# sudo apt-get install libmysqlclient-dev
 
 # other
 # sudo apt-get install htop
